09bea8170c730bef293c6fd9c30fe4c28ff26a7d
Joseph Ghanimah <jfghanimah@gmail.com>
2023-08-11 22:50:21 -0500

    Skeleton Project Initialization

---
+++ social_game_rating/home/static/slideshow.js
@@ -0,0 +1,26 @@
+var images = ["static/game1.jpg", "static/game2.jpg", "static/game3.jpg", "static/game4.jpg"]; // Add more images to this array as needed
+var currentImageIndex = 0;
+
+function changeImage() {
+    var container = document.querySelector(".slideshow-container");
+    
+    // Fade out the current image
+    container.style.opacity = 0;
+    
+    // Wait for the transition to complete (500ms based on the CSS transition duration)
+    setTimeout(function() {
+        // Increment the index and loop back to the start if necessary
+        currentImageIndex++;
+        if (currentImageIndex >= images.length) {
+            currentImageIndex = 0;
+        }
+
+        // Change the background image
+        container.style.backgroundImage = "url('" + images[currentImageIndex] + "')";
+
+        // Fade in the new image
+        container.style.opacity = 1;
+    }, 500); // Match this to the CSS transition duration
+}
+
+setInterval(changeImage, 5000); // Change the image every 7 seconds
